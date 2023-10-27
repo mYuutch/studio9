0591fc71f99ef93315e27d78f03d003e0a4e5e0b
Enzo <enzobouchez434@gmail.com>
2023-10-27 18:49:09 +0200

    new glass texture

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
   <div class="canvas-cont">
     <canvas id="canvas3d">
     </canvas>
+    <canvas id="canvasTrail">
+    </canvas>
   </div>
 </template>
 
 <script>
 import { Application } from '@splinetool/runtime';
 
+
 export default {
   mounted() {
     // Access the document and create Application after the component has been mounted
     const canvas = document.getElementById('canvas3d');
     const app = new Application(canvas);
-    app.load('https://draft.spline.design/nykA0EwxiRpgdncC/scene.splinecode');
+    app.load('https://draft.spline.design/MBnMYtGv-PoDG8-h/scene.splinecode');
   },
 };
 </script>
@@ -25,7 +28,7 @@ body{
 }
 #canvas3d{
   background-color: black;
-  width: 100vw;
-  height: 100vh;
+  width: 50vw;
+  height: 50vh;
 }
 </style>
