5b6d84b4b3a627e98ce2db8e49324a5b7bd3cd59
Enzo <enzobouchez434@gmail.com>
2023-11-19 02:07:01 +0100

    imported local fonts: Gotham[Bold, Medium], Istok[Regular]

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  buildModules: ['@nuxt/content'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -8,4 +9,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  content: {
+    dir: 'assets',
+  },  
 })
