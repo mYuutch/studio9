f71ee04df12bbfe11a335564f349affcc106f39e
Enzo <enzobouchez434@gmail.com>
2023-11-20 19:10:47 +0100

    text behind 3d object

--- components/Hero3D.client.vue
+++ components/Hero3D.client.vue
@@ -1,39 +1,39 @@
   <template>
-<div class="container bg-black my-24 flex w-full mx-auto items-center justify-center px-24 py-16">
-        <h1 class="text-8xl text-white w-2/5 uppercase">Ensemble, vers de <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">nouveaux horizons créatifs</span></h1>
+    <div id="baseline" class=" absolute z-[-1] container box-border bg-black my-24 flex mx-auto items-center justify-center px-24 py-24">
+        <h1 class="text-8xl text-white uppercase">Ensemble, vers de <span class="bg-clip-text bg-gradient-to-r from-blue-700 to-slate-800">nouveaux horizons créatifs</span></h1>
     </div>
   </template>
 
   <script>
 
-  /* import { Application } from '@splinetool/runtime'; */
+    /* import { Application } from '@splinetool/runtime'; */
   import * as THREE from 'three'
-  import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
   import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
   import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'; // Ensure the correct path to RGBELoader
   import Stats from 'three/examples/jsm/libs/stats.module'
-
+  import * as htmlToImage from 'html-to-image'
+  
           
   // INIT SCENE
   const scene = new THREE.Scene()
-  /*scene.add(new THREE.AxesHelper(5))*/
-  //CAMERA
   const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
   )
-
-  camera.position.z = 40
+  camera.position.z = 50
   camera.position.y = 0
   camera.rotation.set(0,0,0)
 
   //LIGHTS
 
+  const light = new THREE.PointLight()
+light.color = new THREE.Color("#999")
+light.power = 800
+light.decay = 0.45
   
   const light1 = new THREE.SpotLight()
-  /*light1.position.copy(camera.position);*/
   light1.position.set(60,0,20)
   light1.rotation.set(0,700,0)
   light1.intensity = 100.0
@@ -41,11 +41,11 @@
   scene.add(light1)
   
 
+  
   const alight = new THREE.AmbientLight( 0x404040 ); // soft white light
   scene.add( alight );
 
-
-  //RENDERER
+  //RENDERERS
   const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true})
   renderer.setClearColor(0x000000,1);
   renderer.shadowMap.enabled = true
@@ -53,16 +53,17 @@
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 
-  //CONTROLS
-  const controls = new OrbitControls(camera, renderer.domElement);
-
-
   //MOUSE POS
-let mouseX = 0, mouseY = 0;
-document.addEventListener('mousemove', function(event) {
-  mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-  mouseY = - (event.clientY / window.innerHeight) * 2 + 1;
-}, false);
+  let mouseX = 0, mouseY = 0;
+  document.addEventListener('mousemove', function(event) {
+    mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    mouseY = - (event.clientY / window.innerHeight) * 2 + 1;
+  }, false);
+
+
+  const divContainer = document.getElementById('baseline');
+
+// Use html2canvas to render the HTML content to a canvas
 
 
   //CHARGEMENT MODEL EXTERNE
@@ -85,14 +86,14 @@ document.addEventListener('mousemove', function(event) {
                   }
                 );
                   const material = new THREE.MeshPhysicalMaterial({
-                    transmission: 1,
+                    transmission: 0.99,
                     thickness: 200,
-                    clearcoat: 0,
-                    metalness:0.1,
-                    clearcoatRoughness: 0,
+                    clearcoat: 1.7,
+                    metalness:0.2,
+                    clearcoatRoughness: 0.3,
                     roughness: 0,
-                    reflectivity: 0.5,
-                    envMapIntensity: 0.4,
+                    reflectivity: 0.9,
+                    envMapIntensity: 0.2,
                     ior: 2.3,
                     side: THREE.DoubleSide,
                     envMap: hdrEquirect
@@ -104,11 +105,26 @@ document.addEventListener('mousemove', function(event) {
       
           pivot.add(group); // Add the group to the pivot
           scene.add(pivot); // Add the pivot to the scene
-          
+        
 
-          
+          setTimeout(() => {
+  htmlToImage.toCanvas(document.getElementById('baseline'), { quality: 1 })
+    .then((canvas) => {
+      const texture = new THREE.CanvasTexture(canvas);
+      const geometry = new THREE.PlaneGeometry(canvas.width, canvas.height);
+      const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+      const plane = new THREE.Mesh(geometry, material);
+      plane.scale.set(0.1,0.1, 0.1)
+      plane.position.set(10, 5, -10);
+      scene.add(plane);
+    })
+    .catch((error) => {
+      console.error('Error rendering HTML to image:', error);
+    });
+}, 1000);
+
          
-          pivot.position.set(0,0,0)
+          pivot.position.set(0,15,0)
 
           const box = new THREE.Box3().setFromObject(group);
           const center = box.getCenter(new THREE.Vector3());
@@ -153,7 +169,7 @@ document.addEventListener('mousemove', function(event) {
       
     }
       requestAnimationFrame(animate)
-      controls.update()
+      /*controls.update()*/
       render()
       stats.update()
   }
@@ -166,3 +182,11 @@ document.addEventListener('mousemove', function(event) {
   animate()
 
     </script>
+
+<style>
+  
+.bg-clip-text {
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+}
+</style>
